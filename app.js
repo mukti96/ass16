580e9a5e2d907d0962697e9e0bb9363a81616da7
mukti96 <mukti.mr1996@gmail.com>
2022-02-05 13:01:13 +0600

    second commit

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ const acc_header = document.querySelectorAll('.accordion-header');
 
 acc_header.forEach(item => {
     item.addEventListener('click', function(){
+        acc_header.forEach( item => {
+
+            if( item != this){
+                item.classList.remove('active');
+                item.nextElementSibling.style.height = '0px'
+            }
+        });
+
         item.classList.toggle('active');
         if(item.classList.contains('active')){
             item.nextElementSibling.style.height = item.nextElementSibling.scrollHeight + 'px'
